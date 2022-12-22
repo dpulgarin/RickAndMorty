c370703cad5101a96285d36b65380e3fe8849347
dgomezp <dgomezp@wabiproject.com>
2022-12-22 09:08:08 +0100

    create WebService

---
+++ app/src/main/java/com/dpulgarin/rickandmorty/application/AppConstants.kt
@@ -0,0 +1,18 @@
+package com.dpulgarin.rickandmorty.application
+
+object AppConstants {
+    // API
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
+
+    // DataBase
+    const val DATABASE_VERSION = 1
+
+    // Socket
+    const val HOSTNAME = "8.8.8.8"
+    const val PORT = 53
+    const val TIMEOUT = 2
+
+    // Alpha
+    const val ALPHA_ENABLED = 1f
+    const val ALPHA_DISABLED = 0.4f
+}
